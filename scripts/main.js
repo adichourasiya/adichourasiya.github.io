d8a49be891d551ec16e75666f10bff85a723ab5c
WCBBC\AC56061 <aditya.chourasiya@worksafebc.com>
2017-09-03 21:22:21 -0700

    added temporary images for removing warning

--- scripts/main.js
+++ scripts/main.js
@@ -44,23 +44,6 @@ function urlB64ToUint8Array(base64String) {
 }
 
 
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-  console.log('Service Worker and Push is supported');
-
-  navigator.serviceWorker.register('sw.js')
-  .then(function(swReg) {
-    console.log('Service Worker is registered', swReg);
-    swRegistration = swReg;
-      initialiseUI();
-  })
-  .catch(function(error) {
-    console.error('Service Worker Error', error);
-  });
-} else {
-  console.warn('Push messaging is not supported');
-  pushButton.textContent = 'Push Not Supported';
-}
-
 function initialiseUI() {
   pushButton.addEventListener('click', function() {
     pushButton.disabled = true;
