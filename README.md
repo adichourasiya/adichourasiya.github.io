c154e41e7115f452dceced8ba654fdd3c9cea2ce
Aditya Chourasiya <AC18633@worksafebc.com>
2019-08-25 19:04:05 -0700

    Redo all

--- README.md
+++
@@ -1,6 +0,0 @@
-# adichourasiya.github.io
-
-It's source code for website adichourasiya.github.io and related components.
-Please read License before using given components. 
-
-Happy coding. 
--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,28 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-        '/',
-        '/images/c (1).jpg',
-        '/images/c (2).jpg',
-        '/images/c (3).jpg',
-        '/images/c (4).jpg',
-        '/css/scrolling-nav.css',
-        '/scripts/main.js',
-        '/js/scrolling-nav.js',
-        '/vendor/jquery-easing/jquery.easing.min.js'
-    ];
+  '/',
+  '/content/css/main-header.css',
+  '/content/images/design.png',
+  '/content/images/develop.png'
+];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
   // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('activate', function (event) {
-    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                    cacheNames.map(function (cacheName) {
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
@@ -37,21 +32,27 @@ self.addEventListener('activate', function (event) {
   );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function (response) {
+      .then(function(response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request);
       }
-                )
-    );
+    )
+  );
 });
 
-const applicationServerPublicKey = 'BFfJYQmEvRi_75UI1LYfAKuGhlCGkAyy09f5OO4GhdcN8fEL0Q_Lt4uzL9sMi9oCabkpQ6x809TI4ahRSni-7ZQ';
+
+/* eslint-disable max-len */
+
+const applicationServerPublicKey = 'BGa5aFaTOtvKhNre55AII7qUtWaOPD6EZlWnon544RZgZBGpO4WV-E--Et-xmiKDVwHs51uUYwd5YFBP0Qkcyaw';
+
+/* eslint-enable max-len */
+
 function urlB64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
@@ -67,18 +68,42 @@ function urlB64ToUint8Array(base64String) {
   return outputArray;
 }
 
-'use strict';
-
 self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Push Codelab';
+  const title = 'Push Notification';
   const options = {
     body: 'Yay it works.',
-    icon: 'images/icon.png',
-    badge: 'images/badge.png'
+    icon: 'content/images/pushNotification/icon.png',
+    badge: 'content/images/pushNotification/badge.png'
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://adichourasiya.github.io')
+  );
+});
+
+
+self.addEventListener('pushsubscriptionchange', function(event) {
+  console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.');
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  event.waitUntil(
+    self.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(function(newSubscription) {
+      // TODO: Send to application server
+      console.log('[Service Worker] New subscription: ', newSubscription);
+    })
+  );
 });
